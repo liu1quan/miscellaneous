ba04672131affa6f5d3dd72c4bc40b00349d14d3
liu1quan <2822557006@qq.com>
2019-12-17 15:34:53 +0800

    修改启动类名称

--- date/src/main/java/com/github/date/Date.java
+++ date/src/main/java/com/github/date/Date.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DateApplication {
+public class Date {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DateApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Date.class, args);
+    }
 
 }
