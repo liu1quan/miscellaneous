c4038c670aba15a77bd49b46f3e43267cf162b56
liu1quan <2822557006@qq.com>
2019-12-17 15:29:51 +0800

    Initial commit

---
+++ image/src/main/java/com/github/image/ImageApplication.java
@@ -0,0 +1,13 @@
+package com.github.image;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Image {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Image.class, args);
+	}
+
+}
