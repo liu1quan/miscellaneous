ca41099ff55aa975389c8991491db307b87be3c9
liu1quan <2822557006@qq.com>
2019-12-17 15:39:19 +0800

    修改启动类名称

--- image/src/main/java/com/github/image/Image.java
+++ image/src/main/java/com/github/image/Image.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Image {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Image.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Image.class, args);
+    }
 
 }
